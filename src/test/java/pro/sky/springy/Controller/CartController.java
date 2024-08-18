63be3557cf3183d5144a06a7a828d4231bfda8a1
freddycooper <almosttanonymous@gmail.com>
2024-08-18 20:51:05 +0300

    homework complete

--- src/test/java/pro/sky/springy/Controller/CartController.java
+++ src/test/java/pro/sky/springy/Controller/CartController.java
@@ -1,4 +1,29 @@
 package pro.sky.springy.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import pro.sky.springy.Service.CartService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/order")
 public class CartController {
+    private final CartService cartService;
+
+    public CartController(CartService cartService) {
+        this.cartService = cartService;
+    }
+
+    @GetMapping("/add")
+    public void add(@RequestParam("id") List<Long> id) {
+        cartService.add(id);
+    }
+
+    @GetMapping("/get")
+    public List<Long> get( ) {
+        return cartService.get();
+    }
 }
--- src/test/java/pro/sky/springy/Service/CartService.java
+++ src/test/java/pro/sky/springy/Service/CartService.java
@@ -1,4 +1,24 @@
 package pro.sky.springy.Service;
 
+import org.springframework.stereotype.Service;
+import pro.sky.springy.ShoppingCart;
+
+import java.util.List;
+
+@Service
 public class CartService {
+    private final ShoppingCart shoppingCart;
+
+    public CartService(ShoppingCart shoppingCart) {
+        this.shoppingCart = shoppingCart;
+    }
+
+    public void add(List<Long> cart) {
+        shoppingCart.add(cart);
+    }
+
+    public List<Long> get() {
+        return shoppingCart.get();
+    }
+
 }
